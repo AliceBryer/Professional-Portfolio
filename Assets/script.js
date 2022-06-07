fcb350c9eb9013acd99d83a498ed47b2badfa193
Alice Bryer <alicebryer123@gmail.com>
2022-06-07 15:08:16 +0100

    card created & footer added

--- Assets/script.js
+++ Assets/script.js
@@ -8,3 +8,11 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+  $(document).ready(function () {
+    //Moment.js code for current date and time
+    let NowMoment = moment().format("MMMM Do YYYY");
+    let displayDate = document.getElementById("currentDay");
+    displayDate.innerHTML = NowMoment;
+    let currentHour = moment().format("HH");
+  })
